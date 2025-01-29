40b2c9fdbbf449b6ac65f10f500393b2f9a6b594
NickDieni <nickevereye@gmail.com>
2025-01-29 12:25:18 +0100

    Updated Controllers

--- my-tec-course.webapi/Controllers/UserReflectionController.cs
+++ my-tec-course.webapi/Controllers/UserReflectionController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using my_tec_course.webapi.Interfaces.Services;
+using my_tec_course.webapi.Models;
 
 namespace my_tec_course.webapi.Controllers
 {
@@ -13,6 +14,67 @@ namespace my_tec_course.webapi.Controllers
         public UserReflectionController(IUserReflectionService userReflectionService)
         {
             _userReflectionService = userReflectionService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] UserReflection userReflection)
+        {
+            try
+            {
+                return Ok(await _userReflectionService.CreateAsync(userReflection));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllUserReflection()
+        {
+            try
+            {
+                return Ok(await _userReflectionService.GetAllAsync());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserReflectionById(int id)
+        {
+            try
+            {
+                return Ok(await _userReflectionService.GetByIdAsync(id));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateUserReflection([FromBody] UserReflection userReflection)
+        {
+            try
+            {
+                return Ok(await _userReflectionService.UpdateAsync(userReflection));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUserReflection(int id)
+        {
+            try
+            {
+                return Ok(await _userReflectionService.DeleteAsync(id));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
     }
 }
