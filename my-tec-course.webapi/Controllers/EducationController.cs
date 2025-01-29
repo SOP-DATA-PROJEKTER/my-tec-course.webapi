0eb8ce327b417122a4f8a8c88f1fa8a21ecaca0b
Jonas Østergaard <Jonasoe@hotmail.com>
2025-01-29 12:20:02 +0100

    Education, Type, Milestone
    Controllers for these

--- my-tec-course.webapi/Controllers/EducationController.cs
+++ my-tec-course.webapi/Controllers/EducationController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using my_tec_course.webapi.Interfaces.Services;
+using my_tec_course.webapi.Models;
 
 namespace my_tec_course.webapi.Controllers
 {
@@ -13,6 +14,72 @@ namespace my_tec_course.webapi.Controllers
         public EducationController(IEducationService educationService)
         {
             _educationService = educationService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                return Ok(await _educationService.GetAllAsync());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                return Ok(await _educationService.GetByIdAsync(id));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Education education)
+        {
+            try
+            {
+                return Ok(await _educationService.CreateAsync(education));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] Education education)
+        {
+            try
+            {
+                return Ok(await _educationService.UpdateAsync(education));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _educationService.DeleteAsync(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
     }
 }
--- my-tec-course.webapi/Controllers/MilestoneController.cs
+++ my-tec-course.webapi/Controllers/MilestoneController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using my_tec_course.webapi.Interfaces.Services;
+using my_tec_course.webapi.Models;
 
 namespace my_tec_course.webapi.Controllers
 {
@@ -13,6 +14,58 @@ namespace my_tec_course.webapi.Controllers
         public MilestoneController(IMilestoneService milestoneService)
         {
             _milestoneService = milestoneService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                return Ok(await _milestoneService.GetAllAsync());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                return Ok(await _milestoneService.GetByIdAsync(id));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Milestone milestone)
+        {
+            try
+            {
+                return Ok(await _milestoneService.CreateAsync(milestone));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] Milestone milestone)
+        {
+            try
+            {
+                return Ok(await _milestoneService.UpdateAsync(milestone));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
     }
 }
