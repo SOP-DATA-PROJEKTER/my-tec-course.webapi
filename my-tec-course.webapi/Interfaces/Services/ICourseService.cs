fdabc86b1b71553425f0cc8c642dab799ee98780
jona339e <98744422+jona339e@users.noreply.github.com>
2025-01-29 11:49:42 +0100

    Revert "Revert 10 9 opret services"

---
+++ my-tec-course.webapi/Interfaces/Services/ICourseService.cs
@@ -0,0 +1,13 @@
+﻿using my_tec_course.webapi.Models;
+
+namespace my_tec_course.webapi.Interfaces.Services
+{
+    public interface ICourseService
+    {
+        Task<IEnumerable<Course>> GetAllCoursesAsync();
+        Task<Course> GetCourseByIdAsync(int id);
+        Task<Course> CreateCourseAsync(Course course);
+        Task<Course> UpdateCourseAsync(int id, Course course);
+        Task<bool> DeleteCourseAsync(int id);
+    }
+}
